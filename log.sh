1e88377c0e5cad8cd0bc047f28f0ddb94c63d2af
Hollow House Institute <data@hollowhouse.org>
2026-04-22 12:15:26 -0500

    log: 2026-04-22_12-14

--- log.sh
+++ log.sh
@@ -1,6 +1,6 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-DATE=$(date +%F)
+DATE=$(date +%F_%H-%M)
 FILE="logs/${DATE}.md"
 
 mkdir -p logs
@@ -14,6 +14,10 @@ echo "" >> $FILE
 echo "## Comment" >> $FILE
 echo "" >> $FILE
 echo "## POST_LOG" >> $FILE
+echo "" >> $FILE
+
+# THIS is the correct way (multi-line paste)
+nano $FILE
 
 git add .
 git commit -m "log: $DATE"
